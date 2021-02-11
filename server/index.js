870221a110ad6d159efbd2312a5f518699ead381
Lauramars <lauramars85@gmail.com>
2021-02-11 15:19:55 +0100

    Fixing network error

--- server/index.js
+++ server/index.js
@@ -9,8 +9,10 @@ app.use(cors());
 
 app.use('/api', require('./route/api'));
 
+app.use(express.static('client/build'));
+
 app.get('*', function (req, res) {
-  res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
