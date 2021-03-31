6d137acd6c10adc740a15eb416062a70583ed340
Lauramars <lauramars85@gmail.com>
2021-03-31 14:50:18 +0200

    Changed index.js

--- server/index.js
+++ server/index.js
@@ -9,8 +9,7 @@ app.use(cors());
 
 app.use('/api', require('./route/api'));
 
-// if (process.env.NODE_ENV === 'production')
-app.use(express.static('../build'));
+if (process.env.NODE_ENV === 'production') app.use(express.static('../build'));
 
 app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
