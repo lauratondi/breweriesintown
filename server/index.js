69a9db5b0132ef462b0e45058ca1b8786df7324e
Lauramars <lauramars85@gmail.com>
2021-03-31 13:54:43 +0200

    from app.get(*) to app.get(/)

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use('/api', require('./route/api'));
 // if (process.env.NODE_ENV === 'production')
 app.use(express.static('client/build'));
 
-app.get('*', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
