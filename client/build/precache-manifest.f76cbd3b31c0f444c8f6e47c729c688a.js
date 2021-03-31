1d85f621dc008bbde5e7bb4e9861a353bd9798b0
Lauramars <lauramars85@gmail.com>
2021-03-31 14:14:19 +0200

    Changed gitignore

---
+++ client/build/precache-manifest.f76cbd3b31c0f444c8f6e47c729c688a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee6504b63012253507e6be0b05ade86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "705253f5e7e175f11a4f",
+    "url": "/static/css/main.ddd8a764.chunk.css"
+  },
+  {
+    "revision": "26c9885d54f3cc489087",
+    "url": "/static/js/2.f6d76ca3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f6d76ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "705253f5e7e175f11a4f",
+    "url": "/static/js/main.b186b9c7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "7ec90a9b18850598ab584b6120c4327f",
+    "url": "/static/media/beers.7ec90a9b.jpg"
+  }
+]);
